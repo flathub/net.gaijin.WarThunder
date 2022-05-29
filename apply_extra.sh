993e42a547d242263ac812c3b2ab6ce2c37210e8
Doomsdayrs <38189170+Doomsdayrs@users.noreply.github.com>
2022-05-29 13:59:58 -0400

    Add logs to apply_extra

--- apply_extra.sh
+++ apply_extra.sh
@@ -4,11 +4,16 @@ SRC="/app/extra/wt.tar.gz"
 XDG_DATA_HOME="/app/extra/tmp/"
 
 # Setup
+echo "Creating directory"
 mkdir -p $XDG_DATA_HOME
 
 # Extract
+echo "Extracting launcherr.dat from wt.tar.gz"
 tar -xv --gzip -f $SRC -C $XDG_DATA_HOME "WarThunder/launcherr.dat"
+
+echo "Extracting version.txt from launcher.dat"
 unzip "$XDG_DATA_HOME/WarThunder/launcherr.dat" -d "/app/extra/" version.txt
 
 # Clean up
+echo "Cleaning up"
 rm -rf $XDG_DATA_HOME
