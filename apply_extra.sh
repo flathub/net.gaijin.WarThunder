8c46a0df401e2bcc5eaf8dd5201e885bf7d6cc10
doomsdayrs <doomsdayrs@gmail.com>
2022-05-28 23:33:29 -0400

    Use tar version.txt instead of yaml defined version.txt
    This solves an issue in which FEDC is unable to update the version.txt declaration in YAML.
    To solve this, this commit reapplies apply_extra, which task is to now extract the version.txt from deep in the tar.
    This will fix the files being extracted over and over again, slowing launch times.

--- apply_extra.sh
+++ apply_extra.sh
@@ -1,17 +1,15 @@
 #!/bin/bash -e
 
-#SRC="/app/extra/wt.tar.gz"
-#XDG_DATA_HOME="/app/extra/tmp/"
+SRC="/app/extra/wt.tar.gz"
+XDG_DATA_HOME="/app/extra/tmp/"
 
 # Setup
-#mkdir -p $XDG_DATA_HOME
+mkdir -p $XDG_DATA_HOME
 
 # Extract
-#tar -xv --gzip -f $SRC -C $XDG_DATA_HOME
-
-# Export icon
-#mkdir -p "/app/extra/export/share/icons/hicolor/128x128/apps/"
-#mv -v "$XDG_DATA_HOME/WarThunder/launcher.ico" "/app/extra/export/share/icons/hicolor/128x128/apps/net.gaijin.WarThunder.png"
+tar -xv --gzip -f $SRC -C $XDG_DATA_HOME "WarThunder/launcherr.dat"
+ls $XDG_DATA_HOME
+unzip "$XDG_DATA_HOME/WarThunder/launcherr.dat" -d "/app/extra/" version.txt
 
 # Clean up
-#rm -rf $XDG_DATA_HOME
+rm -rf $XDG_DATA_HOME
--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -e
 
+# Extra data tar
 SRC="/app/extra/wt.tar.gz"
+
+# Allows for custom install location
 DIR=${CUSTOM_DIR:-$XDG_DATA_HOME}
-FLAT_VERSION="/app/bin/version"
-DATA_VERSION="${DIR}/WarThunder/version"
+
+# Version files to compare
+FLAT_VERSION="/app/extra/version.txt" # Extracted with apply_extra.sh
+DATA_VERSION="${DIR}/WarThunder/version.txt"
 
 # Extract
 if [[ ! -f "$DATA_VERSION" ]] || [[ `cmp --silent "$FLAT_VERSION" "$DATA_VERSION"; echo $?` -ne 0 ]] ; then
