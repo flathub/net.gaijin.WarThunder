a707e19a13e46fee14897880db7b145f33b8ad05
Doomsdayrs <38189170+Doomsdayrs@users.noreply.github.com>
2022-05-29 15:06:11 -0400

    Change tar to bsdtar

--- apply_extra.sh
+++ apply_extra.sh
@@ -9,10 +9,10 @@ mkdir -p $XDG_DATA_HOME
 
 # Extract
 echo "Extracting launcherr.dat from wt.tar.gz"
-tar -xv --gzip -f $SRC -C $XDG_DATA_HOME "WarThunder/launcherr.dat"
+bsdtar -xv --gzip -f $SRC -C $XDG_DATA_HOME "WarThunder/launcherr.dat"
 
 echo "Extracting version.txt from launcher.dat"
-unzip "$XDG_DATA_HOME/WarThunder/launcherr.dat" -d "/app/extra/" version.txt
+bsdtar -f "$XDG_DATA_HOME/WarThunder/launcherr.dat" -d "/app/extra/" version.txt
 
 # Clean up
 echo "Cleaning up"
